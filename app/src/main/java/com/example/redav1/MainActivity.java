9356af8c61f49cd8a2cea9da9537aaeb37d3c41f
ruthshada15 <ruthshada15@gmail.com>
2019-03-06 22:13:23 +0300

    Reda version one. added a temporary splash screen, simple log in screen and choose activity

---
+++ app/src/main/java/com/example/redav1/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.redav1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Thread thread = new Thread()
+        {
+            @Override
+            public void run()
+            {
+             try
+             {
+                sleep(5000);
+             }
+             catch (Exception e)
+             {
+                e.printStackTrace();
+             }
+             finally {
+                 Intent welcomeIntent= new Intent(MainActivity.this,WelcomeActivity.class);
+                 startActivity(welcomeIntent);
+             }
+
+            }
+        };
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        finish();
+    }
+}
